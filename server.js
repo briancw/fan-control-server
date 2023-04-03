235713c2ef7499f6bc483fdcce32e4d6fd318af3
Brian Whicheloe <brian@whicheloe.us>
2023-04-03 15:16:08 -0700

    Add temp view

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ import polka from 'polka'
 import bodyParser from 'body-parser'
 import {exec} from 'node:child_process'
 
+const port = 3000
 let currentSpeed = 0
+let currentTemperature = 0
 
 const setFanSpeed = (speed) => {
     const hexValue = `0x${speed.toString(16)}`
@@ -17,6 +19,7 @@ polka()
 .use(bodyParser.json())
 .post('/', (request, response) => {
     const {temperature} = request.body
+    currentTemperature = temperature
 
     if (temperature > 60) {
         setFanSpeed(100)
@@ -33,7 +36,11 @@ polka()
 
     response.end()
 })
-.listen(3000, (error) => {
+.get('/', (request, response) => {
+    response.end(`Current temperature: ${currentTemperature}°C`)
+})
+.listen(port, (error) => {
+    console.log(`Listening on port ${port}`)
     if (error) {
         console.error(error)
     }
